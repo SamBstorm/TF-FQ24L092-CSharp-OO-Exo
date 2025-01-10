0111a313e5bf1f0fdf84274c369e588498e5cd04
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-10 17:00:57 +0100

    Add Demo IDisposable

---
+++ Demo-IDisposable/Models/TalkieWalkie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_IDisposable.Models
+{
+    public class TalkieWalkie : IDisposable
+    {
+        public string NomUtilisateur { get; set; }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"C'était {NomUtilisateur}, terminé!");
+        }
+
+        public void Parler(string message)
+        {
+            Console.WriteLine($"{NomUtilisateur} : {message}");
+            Console.WriteLine("À vous!");
+        }
+    }
+}
---
+++ Demo-IDisposable/Program.cs
@@ -0,0 +1,30 @@
+﻿using Demo_IDisposable.Models;
+
+namespace Demo_IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TalkieWalkie t1 = new TalkieWalkie() { NomUtilisateur = "Joachim" })
+            {
+                using (TalkieWalkie t2 = new TalkieWalkie() { NomUtilisateur = "Linda" })
+                {
+                    t1.Parler("Bonjour, suis-je seul?");
+                    t2.Parler("Non, je t'écoute!");
+                    t1.Parler("Malheureusement je dois partir! Au revoir.");
+                    t2.Parler("Dommage! Au revoir.");
+                }
+            }
+
+            /* using sans accolades
+            using TalkieWalkie t1 = new TalkieWalkie() { NomUtilisateur = "Joachim" };
+            using TalkieWalkie t2 = new TalkieWalkie() { NomUtilisateur = "Linda" };
+            t1.Parler("Bonjour, suis-je seul?");
+            t2.Parler("Non, je t'écoute!");
+            t1.Parler("Malheureusement je dois partir! Au revoir.");
+            t2.Parler("Dommage! Au revoir.");
+             */
+        }
+    }
+}
